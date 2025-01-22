8302cc1adbd60939709b78f3a6ace9f1a7ef5089
Jure Križman <115313759+jurekrizman@users.noreply.github.com>
2025-01-22 11:37:26 +0100

    Delete uga.js

--- uga.js
+++
@@ -1,8 +0,0 @@
-// Pridobimo gumb in sporočilo
-const button = document.getElementById('actionButton');
-const responseMessage = document.getElementById('responseMessage');
-
-// Dodamo dogodek na gumb
-button.addEventListener('click', () => {
-  responseMessage.classList.toggle('hidden'); // Prikaži ali skrij sporočilo
-});
